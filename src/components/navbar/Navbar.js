ec3c42c0355de552dccce3a503a65f55ed7a7022
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-02-02 15:19:16 +0330

    responsive nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,34 +1,37 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
-import "./navbar.css"
+import "./navbar.css";
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
+  const styleMenu = {
+    top: openMenu ? "7%" : "-100%",
+  }
   return (
     <nav className="navbar">
-      <div className="container">
+      <div className="wrapper">
         <NavLink
           className="toggle-menu"
           to="/"
           onClick={() => setOpenMenu(!openMenu)}
         >
-          {openMenu ?<FaTimes /> : <FaBars />}
+          {openMenu ? <FaTimes /> : <FaBars />}
         </NavLink>
-        <ul className="nav-list">
+        <ul style={styleMenu}>
           <NavLink className="nav-item" to="/">
             خانه
           </NavLink>
-          <NavLink className="nav-item" to="/">
+          <NavLink className="nav-item" to="/product">
             محصولات
           </NavLink>
-          <NavLink className="nav-item" to="/">
+          <NavLink className="nav-item" to="/your-librari">
             کتابخانه شما
           </NavLink>
-          <NavLink className="nav-item" to="/">
+          <NavLink className="nav-item" to="/login">
             ورود
           </NavLink>
-          <NavLink className="nav-item" to="/">
+          <NavLink className="nav-item" to="/register">
             ثبت نام
           </NavLink>
         </ul>
