914023e411f25472f99a09d18bf8275a72075ac2
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-02-03 23:28:47 +0330

    slider with react responsive carousel

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Banner from "./components/banner/Banner";
+import Slider from "./components/slider/Slider";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <BrowserRouter>
         <Navbar />
         <Banner/>
+        <Slider/>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/slider/data.js
@@ -0,0 +1,9 @@
+ const data = [
+  { id: 1, image: "book/1.jpg", description: "کتاب اول" },
+  { id: 2, image: "book/2.jpg", description: "کتاب دوم" },
+  { id: 3, image: "book/3.jpg", description: "کتاب سوم" },
+  { id: 4, image: "book/4.jpg", description: "کتاب چهارم" },
+  { id: 5, image: "book/5.jpg", description: "کتاب پنجم" },
+  { id: 6, image: "book/6.jpg", description: "کتاب ششم" },
+];
+export default data
